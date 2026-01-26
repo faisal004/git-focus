3ba22b04cf90ce14a28a66a9b547ce4e1db0e6f2
Faisal Husain <faisalhusain1320@gmail.com>
2026-01-26 10:27:34 +0530

    checking for auto update

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -38,7 +38,7 @@ function App() {
       <UpdateNotification />
       <div className="main">
         <div style={{ textAlign: 'center', margin: '20px', fontSize: '1.2em', fontWeight: 'bold' }}>
-          System Monitor v0.0.15
+          System Monitor v0.0.19
         </div>
         <div>
           <SelectOption
